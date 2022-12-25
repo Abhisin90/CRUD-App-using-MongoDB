258124a0fb3a37b2725cb34c0611f588bfc51614
glitchsin <abhinavsin90@gmail.com>
2022-12-25 20:14:05 +0530

    Added utility for async errors

--- index.js
+++ index.js
@@ -22,78 +22,64 @@ app.use(methodOverride('_method'))
 
 const categories = ['fruits','vegetables','dairy']
 
-app.get('/products',async (req,res,next) => {
-  try{
-    const {category} = req.query
-    if(category) {
-      const products = await Product.find({category})
-      res.render('products/index',{products,category})
-    }
-    else {
-      const products = await Product.find({})
-      res.render('products/index',{products,category:"All"})
-    }  
-  } catch(e) {
-    next(e)
+function catchAsync(fn) {                      // async utility helps to duplicate try and catch for async erro handling
+  return function (req,res,next) {
+    fn(req,res,next).catch(e => next(e))
   }
-})
+}
+
+app.get('/products', catchAsync(async (req,res) => {
+  const {category} = req.query
+  if(category) {
+    const products = await Product.find({category})
+    res.render('products/index',{products,category})
+  }
+  else {
+    const products = await Product.find({})
+    res.render('products/index',{products,category:"All"})
+  }  
+}))
 
 app.get('/products/new',(req,res) =>{
   res.render('products/new')
 })
 
-app.get('/products/:id',async (req,res,next) => {     // async function ->> next is called automatically when rejected or thrown error,next will call the error handler then
-  try {
-    const {id} = req.params
-    const product = await Product.findById(id)
-    if(!product) {
-      throw new AppError("No such Product",404)      // passing the errot to next
-    }
-    res.render('products/show',{product})
-  } catch(e) {
-    next(e)
+app.get('/products/:id',catchAsync(async (req,res) => {     // async function ->> next is called automatically when rejected or thrown error,next will call the error handler then
+  const {id} = req.params
+  const product = await Product.findById(id)
+  if(!product) {
+    throw new AppError("No such Product",404)      // passing the errot to next
   }
-})
+  res.render('products/show',{product})
+}))
 
-app.get('/products/:id/edit',async (req,res,next) => {
-  try {
-    const {id} = req.params
-    const product = await Product.findById(id)
-    if(!product) {
-      return next(new AppError("No such Product",404))      // passing the error to next
-    }
-    res.render('products/edit',{product,categories})
-  } catch(e) {
-    next(e)
+app.get('/products/:id/edit',catchAsync(async (req,res) => {
+  const {id} = req.params
+  const product = await Product.findById(id)
+  if(!product) {
+    throw new AppError("No such Product",404)  // passing the error to next
   }
-})
+  res.render('products/edit',{product,categories}) 
+}))
 
-app.post('/products', async (req,res,next) => {
+app.post('/products', catchAsync(async (req,res) => {
   //  Product.insertMany(req.body)
-  try{
-    const newProduct = new Product(req.body)
-    await newProduct.save()
-    res.redirect('/products')
-  } catch(err) {
-     next(err)
-  }
-})
+  const newProduct = new Product(req.body)
+  await newProduct.save()
+  res.redirect('/products')
+}))
 
-app.put('/products/:id',async (req,res,next) => {
-  try{
-    const {id} = req.params
-    await Product.findByIdAndUpdate(id,req.body,{runValidators:true})
-    res.redirect(`/products`)
-  } catch(e) {
-    next(e)
-  }
-})
+app.put('/products/:id',catchAsync(async (req,res) => {
+  const {id} = req.params
+  await Product.findByIdAndUpdate(id,req.body,{runValidators:true})
+  res.redirect(`/products`)
+}))
   
-app.delete('/products/:id',async (req,res) => {
+app.delete('/products/:id',catchAsync(async (req,res) => {
     const {id} = req.params
     await Product.findByIdAndDelete(id)
     res.redirect('/products') 
-})
+}))
 
 app.use((err,req,res,next) => {
   const {status=500,message="etege"} = err
